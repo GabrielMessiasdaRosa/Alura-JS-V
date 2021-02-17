3f169dcfa3b19db0ab6807807e848d225fc734f4
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2021-02-17 18:14:36 -0300

    Refatorando o codigo, mudança nos nomes das classes e importando modulos

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-class cliente {
-    nomeCompleto;
-    cpf;
-    rg
-    conta;
-}
-class contaCorrente {
-    agenciaBancaria;
-    _saldoContaCorrente = 0;
+import { Cliente } from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-    sacar(valor) {
-        if (this._saldoContaCorrente >= valor) {
-            this._saldoContaCorrente -= valor;
-            return valor;
-        }
-    }
-    depositar(valor) {
-        if (valor > 0) {
-            this._saldoContaCorrente += valor;
-            return valor;
+    const cliente1 = new Cliente();
 
-        }
-    }
-}
-
-    const cliente1 = new cliente();
-
-    cliente1.nomeCompleto = 'Gabriel Messias'
-    cliente1.cpf = "075.604.589-47"
-    cliente1.rg = "13.858.148-5"
+cliente1.nomeCompleto = 'Gabriel Messias'
+cliente1.cpf = "075.604.589-47"
+cliente1.rg = "13.858.148-5"
 
 
-    const contaCorrenteGabriel = new contaCorrente();
-    contaCorrenteGabriel._saldoContaCorrente;
-    contaCorrenteGabriel.agenciaBancaria = 1001;
+const contaCorrenteGabriel = new ContaCorrente();
+contaCorrenteGabriel._saldoContaCorrente;
+contaCorrenteGabriel.agenciaBancaria = 1001;
 
 
-    console.log(cliente1);
+console.log(cliente1);
 
-    console.log("Seu saldo é : ", contaCorrenteGabriel._saldoContaCorrente);
+console.log("Seu saldo é : ", contaCorrenteGabriel._saldoContaCorrente);
