41dd1c0430dcb00d52ead2a3d6847da4c174c921
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2021-02-18 22:11:59 -0300

    Refatorando o codigo, adicionando novos recursos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,32 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    agenciaBancaria;
+    //atributos publicos
+
+    static numeroDeContas = 0;
+    _agenciaBancaria;
+    cidade = "Curitiba";
+    cliente;
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+            return;
+        }
+        else {
+            return;
+        }
+
+    }
+    get cliente() {
+        return this._cliente;
+    }
+
+    // atributos privados
     _saldoContaCorrente = 0;
+
+    get saldo() {
+        return this._saldoContaCorrente;
+    }
 
     sacar(valor) {
         if (this._saldoContaCorrente >= valor) {
@@ -8,11 +34,24 @@ export class ContaCorrente {
             return valor;
         }
     }
+
     depositar(valor) {
         if (valor > 0) {
             this._saldoContaCorrente += valor;
             return valor;
 
         }
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+
+    constructor(agencia,cliente) {
+        this._agenciaBancaria = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+        return cliente,agencia;
     }
 }
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+//
 import { Cliente } from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
+//
 
-    const cliente1 = new Cliente();
+//
+const cliente1 = new Cliente('Gabriel Messias', "075.604.581-47", "13.858.148-5" );
 
-cliente1.nomeCompleto = 'Gabriel Messias'
-cliente1.cpf = "075.604.589-47"
-cliente1.rg = "13.858.148-5"
+const cliente2 = new Cliente("Joao das Neves","852.852.852-02", "55.555.555-6");
+//
 
+//
+const contaCorrenteGabriel = new ContaCorrente(1001, cliente1);
 
-const contaCorrenteGabriel = new ContaCorrente();
-contaCorrenteGabriel._saldoContaCorrente;
-contaCorrenteGabriel.agenciaBancaria = 1001;
+const contaCorrenteJoao = new ContaCorrente(1002, cliente2);
 
+//
 
-console.log(cliente1);
+//
 
-console.log("Seu saldo é : ", contaCorrenteGabriel._saldoContaCorrente);
+contaCorrenteGabriel.depositar(180);
+contaCorrenteGabriel.transferir(75, contaCorrenteJoao);
+contaCorrenteGabriel.depositar(1027.12);
+//
+
+//
+console.log(contaCorrenteGabriel);
+console.log("Ola", cliente1.nomeCompleto, "\nSeu saldo é : ", contaCorrenteGabriel.saldo);
+console.log("____________________________________________\n")
+console.log(contaCorrenteJoao);
+console.log("Ola", cliente2.nomeCompleto, "\nSeu saldo é : ", contaCorrenteJoao.saldo,"\n");
+console.log(ContaCorrente.numeroDeContas)
